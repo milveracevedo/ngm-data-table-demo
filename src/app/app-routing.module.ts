8dca38714c62aee5e05f6a0ed5fd04fd2fafb2a1
milveracevedo <milverfloresacevedo@gmail.com>
2019-12-25 09:42:23 -0400

    4 files modified, 23 added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TemplateComponent } from './cell/template/template.component';
+import { HeaderExtraComponent } from './header/header-extra/header-extra.component';
+import { HeaderTemplateComponent } from './header/header-template/header-template.component';
+import { CustomIconExpandComponent } from './responsive/custom-icon-expand/custom-icon-expand.component';
+import { DefaultComponent } from './responsive/default/default.component';
+import { TemplateExpandComponent } from './responsive/template-expand/template-expand.component';
+import {SimpleComponent} from "./simple/simple/simple.component";
+
+const routes: Routes = [
+  { path: 'simple', component: SimpleComponent },
+  { path: 'header/header-template', component: HeaderTemplateComponent },
+  { path: 'header/header-template-extra', component: HeaderExtraComponent },
+  { path: 'cell/cell-template', component: TemplateComponent },
+  { path: 'responsive/default', component: DefaultComponent },
+  { path: 'responsive/template', component: TemplateExpandComponent },
+  { path: 'responsive/custom-icon-expand', component: CustomIconExpandComponent },
+  { path: '**', component: SimpleComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(
+    routes, {
+    useHash: true
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgmDataTableModule } from 'ngm-data-table';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { TemplateComponent } from './cell/template/template.component';
+import { HeaderExtraComponent } from './header/header-extra/header-extra.component';
+import { HeaderTemplateComponent } from './header/header-template/header-template.component';
+import { CustomIconExpandComponent } from './responsive/custom-icon-expand/custom-icon-expand.component';
+import { DefaultComponent } from './responsive/default/default.component';
+import { TemplateExpandComponent } from './responsive/template-expand/template-expand.component';
+import { SimpleComponent } from './simple/simple/simple.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    SimpleComponent,
+    DefaultComponent,
+    TemplateExpandComponent,
+    HeaderExtraComponent,
+    HeaderTemplateComponent,
+    TemplateComponent,
+    CustomIconExpandComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    NgmDataTableModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
